e952777dd56af483f421bb67ddaae96aed6c44aa
Lauralalulul1 <68302698+Lauralalulul1@users.noreply.github.com>
2020-11-04 19:11:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,83 @@
-// from data.js
-var tableData = data;
-
-// YOU// Reference: https://www.youtube.com/watch?v=C4t6qfHZ6Tw, Slack, Activity Lessons
+// Reference: https://www.youtube.com/watch?v=C4t6qfHZ6Tw, Slack, Activity Lessons
 // Create a basic HTML web page or use the index.html file provided (we recommend building your own custom page!).
 
 //Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data 
 //for each UFO sighting.
 
 // from data.js
-var tableData = data;
+var tableData = d3.select ("#data");
 
-// Use D3 to select the table body
+// Get a reference to the table body
 var tbody = d3.select("tbody");
 
-//Select the button
-var button = d3.select("#button");
+// Console.log data from data.js
+console.log(data)
 
-//More references
-var inputFieldDate = d3.select("#datetime");
-var inputFieldCity = d3.select("#city");
-
-//Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
+//Columns for date/time, city, state, country, shape, and comment at the very least.
 //slack/10.21 from class activities
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
 
-//Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
-// learn in class 10.25
-var addData = (dataInput) => {
-    dataInput.forEach(ufoSightings => {
-        var row = tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSightings[column]))
-   });
+function createDefault() {
+    ufoSightings.forEach(ufoSightings => {
+        row = tableBody.append("tr")
+        row.append("tr").text(date);
+        row.append("tr").text(city);
+        row.append("tr").text(state);
+        row.append("tr").text(Country);
+        row.append("tr").text (Shape);
+        row.append("tr").text (Duration);
+        row.append("tr").text (Comments);
+
+    });
 }
-   addData(tableData);
-// create the filter button to make it cliickable
-button.on('click", () => {
+createDefault();
 
-// Prevent the page from refreshing
-    d3.event.preventDefault();
+//Create reference for the button and date
+var button = d3.select("#filter-btn")
+var resetButton = d3.select("#resetButton")
+var datetime = d3.select("#datetime")
 
-//Learn 10.25 filters Filter Data with datetime equal to input value
- // Get the value property of the input element
-    var inputDate = inputFieldDate.property("value").trim();
-    
-    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
-     
-    tbody.html("");
+resetButton.on("click", () => {
+    tableBody.remove();
+    tableBody = table.append("tbody");
+    createDefault();
+});
 
-    let response = {
-        filterDate
+//code that will listen for events and search through the date/time column to 
+//find rows that match user input.
 
-    if (response.filterDate.length !== 0) {
-       addData(filterData);
+seriesSearchField.on("keyup", () => {
+    var seriesToSearchFor = seriesSearchField.property("value");
+    if (seriesToSearchFor.length >= 7) {
+        tableBody.remove();
+        tableBody = table.append("tbody");
+
+        fighters.filter(f => f.series.toLowerCase().trim() == seriesToSearchFor.toLowerCase().trim())
+            .forEach(fighter => {
+                var row = tableBody.append("tr");
+                  row.append("tr").text(date);
+                  row.append("tr").text(city);
+                  row.append("tr").text(state);
+                  row.append("tr").text(Country);
+                  row.append("tr").text (Shape);
+                  row.append("tr").text (Duration);
+                  row.append("tr").text (Comments);
+
+            });
+
     }
+});
 
-   
-    else {
-       tbody.append("tr").append("td").text("No aliens. Continue... ");
-    }
-    })
-R CODE HERE!
+characterSearchButton.on("click", () => {
+    var characterToSearchFor = characterSearchField.property("value");
+    console.log(characterToSearchFor);
+    tableBody.remove();
+    tableBody = table.append("tbody");
+
+    fighters.filter(f => f.displayNameEn.toLowerCase().trim() == characterToSearchFor.toLowerCase().trim())
+        .forEach(fighter => {
+            var row = tableBody.append("tr");
+            row.append("td").text(fighter.id);
+            row.append("td").text(fighter.displayNameEn);
+            row.append("td").text(fighter.series);
+        });
