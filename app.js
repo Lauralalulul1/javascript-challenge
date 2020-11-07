9bc6847b9dcadb3ae135cfcc5a85d4905e455b0a
Lauralalulul1 <68302698+Lauralalulul1@users.noreply.github.com>
2020-11-07 14:44:29 -0600

    Update app.js

--- app.js
+++ app.js
@@ -17,67 +17,42 @@ console.log(data)
 //slack/10.21 from class activities
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
 
-function createDefault() {
-    ufoSightings.forEach(ufoSightings => {
-        row = tableBody.append("tr")
-        row.append("tr").text(date);
-        row.append("tr").text(city);
-        row.append("tr").text(state);
-        row.append("tr").text(Country);
-        row.append("tr").text (Shape);
-        row.append("tr").text (Duration);
-        row.append("tr").text (Comments);
+//Create reference for the button and make button clickable
+var button = d3.select("#filter-btn");
+var resetbutton= d3.select("resetbutton");
 
+button.on("click", UFOdata);
+resetbutton.on("submit", UFOdata");
+
+//Create filter
+function UFOdata() {
+    d3.event.preventDefault();
+    const datetimefilter = d3.select("#datetime").property("value");
+    var results = tableData.filter((ufo) => ufo["datetime"] == datetimefilter);
+
+    renderTable(results);
+}
+
+function renderTable(table);
+    var tbody = d3.select("tbody");
+    tbody.html("");
+    table.forEach((sightings) => (
+        var row = tbody.append("tr");
+      //console.log(sightings);
+      for (let key in sightings) {
+        var cell = row.append("td");
+        cell.text(sightings[key]);
+        
+      }
     });
 }
-createDefault();
 
-//Create reference for the button and date
-var button = d3.select("#filter-btn")
-var resetButton = d3.select("#resetButton")
-var datetime = d3.select("#datetime")
+renderTable(data);
 
-resetButton.on("click", () => {
-    tableBody.remove();
-    tableBody = table.append("tbody");
-    createDefault();
-});
+    
+   
+    
+    
+    
 
-//code that will listen for events and search through the date/time column to 
-//find rows that match user input.
-
-seriesSearchField.on("keyup", () => {
-    var seriesToSearchFor = seriesSearchField.property("value");
-    if (seriesToSearchFor.length >= 7) {
-        tableBody.remove();
-        tableBody = table.append("tbody");
-
-        fighters.filter(f => f.series.toLowerCase().trim() == seriesToSearchFor.toLowerCase().trim())
-            .forEach(fighter => {
-                var row = tableBody.append("tr");
-                  row.append("tr").text(date);
-                  row.append("tr").text(city);
-                  row.append("tr").text(state);
-                  row.append("tr").text(Country);
-                  row.append("tr").text (Shape);
-                  row.append("tr").text (Duration);
-                  row.append("tr").text (Comments);
-
-            });
-
-    }
-});
-
-characterSearchButton.on("click", () => {
-    var characterToSearchFor = characterSearchField.property("value");
-    console.log(characterToSearchFor);
-    tableBody.remove();
-    tableBody = table.append("tbody");
-
-    fighters.filter(f => f.displayNameEn.toLowerCase().trim() == characterToSearchFor.toLowerCase().trim())
-        .forEach(fighter => {
-            var row = tableBody.append("tr");
-            row.append("td").text(fighter.id);
-            row.append("td").text(fighter.displayNameEn);
-            row.append("td").text(fighter.series);
-        });
+        
